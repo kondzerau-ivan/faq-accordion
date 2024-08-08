3a74a8cf841d562306ba0c5d37033578f7da5c2d
Ivan Kondzerau <kondzerau.ivan@gmail.com>
2024-08-09 00:24:13 +0300

    add fonts, reset styles, add custom properties

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <>
     <h1>start</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam omnis adipisci cum neque illo sed eos aspernatur, perspiciatis necessitatibus modi exercitationem rerum, eaque vel accusamus quo, voluptatem odio illum eum.</p>
+    </>
   )
 }
 
